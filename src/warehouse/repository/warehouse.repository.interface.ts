e374b1929c75ec5074633f49eaba8baeafd841ae
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-06-01 15:12:21 -0300

    refactor: remove unnecessary method

--- src/warehouse/repository/warehouse.repository.interface.ts
+++ src/warehouse/repository/warehouse.repository.interface.ts
@@ -7,7 +7,6 @@ export abstract class IWarehouseRepository {
   abstract create(warehouse: WarehouseInput): Promise<WarehouseDTO>;
   abstract findAll(): Promise<WarehouseDTO[]>;
   abstract findByProviderId(providerId: number): Promise<WarehouseDTO[]>;
-  abstract findByProviderId(providerId: number): Promise<WarehouseDTO[]>;
   abstract findById(id: number): Promise<WarehouseDTO>;
   abstract moveProduct(
     productId: number,
